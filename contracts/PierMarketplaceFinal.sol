30964c5ebef3345fda2db0fecdd498190be8effd
konan-5 <ikedahiroshi517@gmail.com>
2024-01-23 14:33:45 +0900

    update friend fee logic

--- contracts/PierMarketplaceFinal.sol
+++ contracts/PierMarketplaceFinal.sol
@@ -1,15 +1,18 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity ^0.8.20;
 
+// Importing necessary OpenZeppelin contracts for token interface, ownership, security, and safe token operations.
 import "@openzeppelin/contracts/interfaces/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
+// PierMarketplace contract inheriting from Ownable and ReentrancyGuard for ownership management and reentrancy protection.
 contract PierMarketplace is Ownable, ReentrancyGuard {
+    // SafeERC20 library usage for safe token operations.
     using SafeERC20 for IERC20;
 
+    // Book struct to hold details about a listing in the marketplace.
     struct Book {
         address seller;
         address sellTokenAddress;
@@ -19,6 +22,7 @@ contract PierMarketplace is Ownable, ReentrancyGuard {
         bool isActive;
     }
 
+    // Struct to hold details about listings priced in ETH.
     struct BookForETH {
         address seller;
         address sellTokenAddress;
@@ -27,37 +31,23 @@ contract PierMarketplace is Ownable, ReentrancyGuard {
         bool isActive;
     }
 
-    struct FriendToken {
-        address tokenAddress;
-        uint256 feeRate;
-        bool isActive;
-    }
-
-    // Total number of listings
+    // State variables to track counts of different listing types and friend tokens.
     uint256 public bookCount = 0;
     uint256 public bookForETHCount = 0;
-    uint256 public friendTokenCount = 0;
-
     address public feeWallet;
 
-    // mapping of all book listings
+    // Mappings for different types of listings and friend tokens.
     mapping(uint256 => Book) public bookList;
-    // mapping of all FriendToken list
-    mapping(uint256 => FriendToken) public friendTokenList;
+    mapping(address => uint8) public friendTokenFeeList;
 
-    // Blacklist mapping
-    mapping(address => bool) private blackList;
-
-    //Events
+    // Events to log various actions and changes on the contract.
     event Booked(uint256 indexed bookId, address indexed seller, address indexed sellTokenAddress, uint256 sellTokenAmount, address paymentTokenAddress, uint256 paymentTokenAmount);
     event TokenPurchased(uint256 indexed bookId, address indexed buyer);
     event BookRemoved(uint256 indexed bookId);
-    event FriendTokenAdded(address indexed tokenAddress, uint256 indexed feeRate);
-    event FriendTokenRemoved(uint256 indexed friendTokenId);
+    event FriendTokenUpdated(address indexed tokenAddress, uint256 indexed feeRate);
     event FeeWalletAddressUpdated(address indexed feeWallet);
 
-
-    //Errors
+    // Custom errors for handling specific fail conditions.
     error InvalidSellTokenAmount(uint256 amount);
     error InvalidPaymentTokenAmount(uint256 priceInWei);
     error InvalidSellTokenAddress(address tokenAddress);
@@ -68,32 +58,31 @@ contract PierMarketplace is Ownable, ReentrancyGuard {
     error InsufficientAllowanceOfSeller(uint256 sellerAllowance, uint256 sellTokenAmount);
     error InsufficientAmountOfSeller(uint256 sellerBalance, uint256 sellTokenAmount);
     error OnlyTheSellerCanRemoveTheBook(address seller, address msgSender);
-    error FriendTokeDoesNotExist(uint256 friendTokenId);
     error InvalidFriendTokenAddress(address tokenAddress);
     error InvalidFeeRate(uint256 feeRate);
     error InvalidFeeWalletAddress(address feeWallet);
     error InvalidPurchasePercent(uint256 purchasePercent);
 
-
+    // Constructor to set the fee wallet address on deployment.
     constructor(address _feeWallet) Ownable(msg.sender) {
         feeWallet = _feeWallet;
     }
 
-    /* PUBLIC FUNCTIONS */
-
+    // Function to list a token for sale in exchange for another token.
     function book(address sellTokenAddress, uint256 sellTokenAmount, address paymentTokenAddress, uint256 paymentTokenAmount) external nonReentrant {
+        // Validation checks for the inputs.
         if (sellTokenAmount == 0) revert InvalidSellTokenAmount(sellTokenAmount);
         if (paymentTokenAmount == 0) revert InvalidPaymentTokenAmount(paymentTokenAmount);
         if (sellTokenAddress == address(0)) revert InvalidSellTokenAddress(sellTokenAddress);
         if (paymentTokenAddress == address(0)) revert InvalidPaymentTokenAddress(paymentTokenAddress);
+
+        // Checking the allowance of the marketplace contract to handle seller's tokens.
         address sender = msg.sender;
         uint256 allowance = IERC20(sellTokenAddress).allowance(sender, address(this));
         require(allowance >= sellTokenAmount, "Marketplace does not have enough allowance to transfer tokens");
 
-        // increase book count
+        // Adding the listing to the book list.
         bookCount++;
-
-        // Add listing to bookList map
         bookList[bookCount] = Book(
             sender,
             sellTokenAddress,
@@ -103,132 +92,106 @@ contract PierMarketplace is Ownable, ReentrancyGuard {
             true
         );
 
-        // Emit book event
+        // Emitting an event for the new listing.
         emit Booked(bookCount, sender, sellTokenAddress, sellTokenAmount, paymentTokenAddress, paymentTokenAmount);
     }
 
+    // Function to buy tokens from a listing.
     function buyToken(uint256 bookId, uint256 purchasePercent) external nonReentrant {
+        // Validation checks for the purchase percent.
         if (purchasePercent == 0 || purchasePercent > 100) revert InvalidPurchasePercent(purchasePercent);
-        // Get listing from mapping
-        Book memory bookItem = bookList[bookId];
-        // Check if listing exists
-        if (bookItem.paymentTokenAddress == address(0)) revert ListingDoesNotExist(bookId);
 
-        // Check is listing is active
+        // Retrieving the book item and performing checks.
+        Book memory bookItem = bookList[bookId];
+        if (bookItem.paymentTokenAddress == address(0)) revert ListingDoesNotExist(bookId);
         if (!bookItem.isActive) revert ListingDoesNotExist(bookId);
 
+        // Calculating the amount of tokens to be transferred based on the purchase percent.
         uint256 paymentTokenAmount = bookItem.paymentTokenAmount * purchasePercent / 100;
         uint256 sellTokenAmount = bookItem.sellTokenAmount * purchasePercent / 100;
 
-        // Check if buyer has approved paymentToken to the marketplace
+        // Checking buyer's allowance and balance for the payment token.
         uint256 buyerAllowance = IERC20(bookItem.paymentTokenAddress).allowance(msg.sender, address(this));
         if (buyerAllowance < paymentTokenAmount) revert InsufficientAllowanceOfBuyer(buyerAllowance, paymentTokenAmount);
-
-        // Check if buyer has enough balance of paymentToken
         uint256 buyerBalance = IERC20(bookItem.paymentTokenAddress).balanceOf(msg.sender);
         if (buyerBalance < paymentTokenAmount) revert InsufficientBalanceOfBuyer(buyerBalance, paymentTokenAmount);
 
-        // Check if seller has approved sellToken to the marketplace
+        // Checking seller's allowance and balance for the sell token.
         uint256 sellerAllowance = IERC20(bookItem.sellTokenAddress).allowance(bookItem.seller, address(this));
         if (sellerAllowance < sellTokenAmount) revert InsufficientAllowanceOfSeller(sellerAllowance, sellTokenAmount);
-
-        // Check if the seller has enough tokens to sell.
         uint256 sellerBalance = IERC20(bookItem.sellTokenAddress).balanceOf(bookItem.seller);
         if (sellerBalance < sellTokenAmount) revert InsufficientAmountOfSeller(sellerBalance, sellTokenAmount);
 
-        //Set listing isActive=false
-        if(purchasePercent == 100)
-        {
-            bookItem.isActive = false;
-        }else {
-            bookItem.paymentTokenAmount -= paymentTokenAmount;
-            bookItem.sellTokenAmount -= sellTokenAmount;
+        // Updating the book item's status or amounts based on the purchase percent.
+        if (purchasePercent == 100) {
+            bookList[bookId].isActive = false;
+        } else {
+            bookList[bookId].paymentTokenAmount -= paymentTokenAmount;
+            bookList[bookId].sellTokenAmount -= sellTokenAmount;
         }
 
-        // Transfer the tokens from the seller to the buyer.
+        // Transferring the sell token from the seller to the buyer.
         IERC20(bookItem.sellTokenAddress).safeTransferFrom(bookItem.seller, msg.sender, sellTokenAmount);
 
-        // Calculate the fee. seller pays for the fee.
-        // (uint256 feeForStaking, uint256 feeForWallet) = _calculateFee(listing.priceInWei, listing.tokenAddress);
-
-        uint256 fee = _calculateFee(bookItem);
-
-        // Transfer the total fee to this contract
+        // Calculating and transferring the fee to the fee wallet.
+        uint256 fee = _calculateFee(bookItem.paymentTokenAddress, bookItem.sellTokenAddress, paymentTokenAmount);
         IERC20(bookItem.paymentTokenAddress).safeTransferFrom(msg.sender, feeWallet, fee);
 
-        // Transfer the remaining amount to the seller
+        // Transferring the remaining payment amount to the seller.
         uint256 amountToSeller = paymentTokenAmount - fee;
-
         IERC20(bookItem.paymentTokenAddress).safeTransferFrom(msg.sender, bookItem.seller, amountToSeller);
 
-        // Emit the TokenPurchased event.
+        // Emitting an event for the token purchase.
         emit TokenPurchased(bookId, msg.sender);
     }
 
+    // Function to remove a book (listing) from the marketplace.
     function removeBook(uint256 bookId) external {
-        // Get listing from mapping
+        // Retrieving the book item and checking if the sender is the seller.
         Book memory bookItem = bookList[bookId];
-
-        // Check if listing exists
         if (bookItem.sellTokenAddress == address(0)) revert ListingDoesNotExist(bookId);
-
-        // Check if msg.sender is the seller
         if (msg.sender != bookItem.seller) revert OnlyTheSellerCanRemoveTheBook(bookItem.seller, msg.sender);
 
-        //Set listing isActive=false
+        // Marking the book as inactive.
         bookList[bookId].isActive = false;
 
-        // Emit event
+        // Emitting an event for the book removal.
         emit BookRemoved(bookId);
     }
 
-    /* Friendship Token */
-
-    function addFriendToken(address tokenAddress, uint256 feeRate) external onlyOwner {
+    // Function for the owner to add a friend token with a specific fee rate.
+    function updateFriendToken(address tokenAddress, uint8 feeRate) external onlyOwner {
+        // Validation checks for the token address and fee rate.
         if (tokenAddress == address(0)) revert InvalidFriendTokenAddress(tokenAddress);
         if (feeRate > 100) revert InvalidFeeRate(feeRate);
-        friendTokenCount ++;
-        friendTokenList[friendTokenCount] = FriendToken (
-            tokenAddress,
-            feeRate,
-            true
-        );
 
-        emit FriendTokenAdded(tokenAddress, feeRate);
+        // Adding the friend token to the list.
+        friendTokenFeeList[tokenAddress] = feeRate;
+
+        // Emitting an event for the new friend token.
+        emit FriendTokenUpdated(tokenAddress, feeRate);
     }
 
-    function removeFriendToken(uint256 friendTokenId) external onlyOwner {
-        FriendToken memory friendToken = friendTokenList[friendTokenId];
-        if (friendToken.tokenAddress == address(0)) revert FriendTokeDoesNotExist(friendTokenId);
-        friendToken.isActive = false;
-
-        emit FriendTokenRemoved(friendTokenId);
-    }
-
+    // Function to update the fee wallet address, restricted to the contract owner.
     function updateFeeWallet(address walletAddress) external onlyOwner {
+        // Validate the new wallet address.
         if (walletAddress == address(0)) revert InvalidFeeWalletAddress(walletAddress);
+
+        // Update the fee wallet address.
         feeWallet = walletAddress;
+
+        // Emit an event to log the update of the fee wallet address.
         emit FeeWalletAddressUpdated(walletAddress);
     }
 
-    /* INTERNAL FUNCTIONS */
+    // Internal function to calculate the fee for a transaction.
+    function _calculateFee(address paymentTokenAddress, address sellTokenAddress, uint256 paymentTokenAmount) internal view returns (uint256) {
+        // Start with a base fee calculation: 3% of the payment token amount.
+        uint256 fee = paymentTokenAmount * 3 / 100;
 
-    function _calculateFee(Book memory bookItem) internal view returns (uint256) {
-        // If the token being transacted is pierToken, return zero fees
-        uint256 fee = bookItem.paymentTokenAmount * 3 / 100;
-        for(uint8 idx = 1; idx <= friendTokenCount; idx ++) {
-            FriendToken memory friendToken = friendTokenList[idx];
-            if(!friendToken.isActive) {
-                continue;
-            }
-            if(bookItem.paymentTokenAddress == friendToken.tokenAddress) {
-                fee = fee * friendToken.feeRate / 100;
-            }
-            if(bookItem.sellTokenAddress == friendToken.tokenAddress) {
-                fee = fee * friendToken.feeRate / 100;
-            }
-            if (fee == 0) return 0;
-        }
+        fee = fee * (100 - friendTokenFeeList[paymentTokenAddress]) / 100;
+        fee = fee * (100 - friendTokenFeeList[sellTokenAddress]) / 100;
+        
         return fee;
     }
 }
